04332dd3290caed9dd383101b82edc992f677aba
Juan Ojeda <juan@juanojeda.com>
2022-04-22 17:58:16 +1000

    convert to typescript, fix errors

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const DOMRoot = document.getElementById("root");
+
+if (DOMRoot) {
+  const root = ReactDOM.createRoot(DOMRoot);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
