3a149f12276cdda3438255a5711518a9e40d57a1
Juan Ojeda <juan@juanojeda.com>
2022-05-18 21:39:23 +1000

    remove unneeded index file, wrap page in layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import { Layout } from "../components/Layout";
 import RecipeList from "../containers/RecipeList";
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>With Linaria</title>
       </Head>
       <RecipeList />
-    </>
+    </Layout>
   );
 }
